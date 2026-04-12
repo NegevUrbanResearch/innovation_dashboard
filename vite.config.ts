e5f6331e58f67373303a9b18e58beebfdd0abb12
noamjgal <noamjgal@gmail.com>
2026-04-12 22:26:24 +0300

    deploy: pages site

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+/// <reference types="node" />
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || "/",
   build: {
     rollupOptions: {
       output: {
